e8e19b7d895b2e0bd58d8d11cd7eef6bdcd00b67
Harel Natan <harelnatan7@gmail.com>
2022-12-16 16:16:50 +0200

    Knisat Shabbat

--- js/memeService.js
+++ js/memeService.js
@@ -8,31 +8,30 @@ let gCurrFontSize
 let gIdLine = 0
 
 const gImgs = [
-    { id: 1, url: 'meme-imgs/1.jpg', keywords: ['trump', 'angry'] },
-    { id: 2, url: 'meme-imgs/2.jpg', keywords: ['cute', 'dogs', 'pets'] },
-    { id: 3, url: 'meme-imgs/3.jpg', keywords: ['baby', 'cute', 'dogs', 'pets'] },
-    { id: 4, url: 'meme-imgs/4.jpg', keywords: ['cute', 'cat', 'pets', 'sleep'] },
-    { id: 5, url: 'meme-imgs/5.jpg', keywords: ['cute', 'baby', 'angry'] },
+    { id: 1, url: 'meme-imgs/1.jpg', keywords: ['di caprio', 'smile', 'laugh', 'wine', 'suit'] },
+    { id: 2, url: 'meme-imgs/2.jpg', keywords: ['old', 'glasses', 'funny', 'white'] },
+    { id: 3, url: 'meme-imgs/3.jpg', keywords: ['baby', 'cute', 'cats', 'pets', 'eyes', 'shrek'] },
+    { id: 4, url: 'meme-imgs/4.jpg', keywords: ['yell', 'blonde', 'point', 'finger', 'mouth'] },
+    { id: 5, url: 'meme-imgs/5.jpg', keywords: ['cute', 'angry', 'cats', 'pets', 'brown'] },
     { id: 6, url: 'meme-imgs/6.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 7, url: 'meme-imgs/7.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 8, url: 'meme-imgs/8.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 9, url: 'meme-imgs/9.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 10, url: 'meme-imgs/10.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 11, url: 'meme-imgs/11.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 12, url: 'meme-imgs/12.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 13, url: 'meme-imgs/13.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 14, url: 'meme-imgs/14.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 15, url: 'meme-imgs/15.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 16, url: 'meme-imgs/16.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 17, url: 'meme-imgs/17.jpg', keywords: ['smart', 'explain', 'history'] },
-    { id: 18, url: 'meme-imgs/18.jpg', keywords: ['smart', 'explain', 'history'] }
+    { id: 7, url: 'meme-imgs/7.jpg', keywords: ['baby', 'cute', 'black', 'suprise'] },
+    { id: 8, url: 'meme-imgs/8.jpg', keywords: ['smart', 'smile', 'hat', 'purple'] },
+    { id: 9, url: 'meme-imgs/9.jpg', keywords: ['cute', 'baby', 'laugh', 'angry'] },
+    { id: 10, url: 'meme-imgs/10.jpg', keywords: ['celeb', 'obama', 'smile', 'laugh'] },
+    { id: 11, url: 'meme-imgs/11.jpg', keywords: ['celeb', 'black', 'basketball', 'kiss'] },
+    { id: 12, url: 'meme-imgs/12.jpg', keywords: ['celeb', 'finger', 'point', 'old'] },
+    { id: 13, url: 'meme-imgs/13.jpg', keywords: ['celeb', 'di caprio', 'smile', 'wine', 'suit'] },
+    { id: 14, url: 'meme-imgs/14.jpg', keywords: ['black', 'matrix', 'glasses', 'angry'] },
+    { id: 15, url: 'meme-imgs/15.jpg', keywords: ['celeb', 'game of thrones', 'stark', 'explain'] },
+    { id: 16, url: 'meme-imgs/16.jpg', keywords: ['celeb', 'laugh', 'red', 'marvel', 'doctor x'] },
+    { id: 17, url: 'meme-imgs/17.jpg', keywords: ['celeb', 'putin', 'two', 'finger', 'suit', 'red'] },
+    { id: 18, url: 'meme-imgs/18.jpg', keywords: ['toy story', 'dolls', 'baz', 'woody', 'green', 'movie']},
+    { id: 19, url: 'meme-imgs/19.jpg', keywords: ['trump', 'angry', 'celeb'] },
+    { id: 20, url: 'meme-imgs/20.jpg', keywords: ['cute', 'dogs', 'pets'] },
+    { id: 21, url: 'meme-imgs/21.jpg', keywords: ['baby', 'cute', 'dogs', 'pets'] },
+    { id: 22, url: 'meme-imgs/22.jpg', keywords: ['cute', 'cat', 'pets', 'sleep'] },
+    { id: 23, url: 'meme-imgs/23.jpg', keywords: ['cute', 'baby', 'angry'] }
 ]
-
-function onSearch(val) {
-    console.log('val:', val)
-    let filteredImgs = gImgs.filter(img => img.keywords.some(keyword => keyword.includes(val)))
-    renderGallery(filteredImgs)
-}
 
 function createMeme(img) {
     let elCanvas = getElCanvas()
@@ -106,6 +105,12 @@ function getImgs() {
 
 function getImg() {
     return gImgs.find(img => gMeme.selectedImgId === img.id)
+}
+
+function onSearch(val) {
+    val = val.toLowerCase()
+    let filteredImgs = gImgs.filter(img => img.keywords.some(keyword => keyword.includes(val)))
+    renderGallery(filteredImgs)
 }
 
 function setFontColor(val) {
